33763f7f063f67825b4d951524e895faa8b8242f
Lauri Sarap <lauri.aardla@gmail.com>
2025-12-17 23:46:11 +0100

    4 complete

--- first_semester/programmierpraktikum/avr/04_taster_pollen/p.c
+++ first_semester/programmierpraktikum/avr/04_taster_pollen/p.c
@@ -1,4 +1,19 @@
 #include <avr/io.h>
 
 int main(void) {
+    DDRD |= (1 << PD0);
+    PORTD = 0xFE;
+
+    char i = 'f';
+
+    while(1) {
+        if (!((1 << PD3) & PIND)) {
+            if (i == 'f') {
+                PORTD ^= (1<<PD0);
+                i = 'o';
+            }
+        } else {
+            i = 'f';
+        }
+    }
 }
