37454b06c1f525c166b05748c2269d31604241d7
Lauri Sarap <lauri.aardla@gmail.com>
2026-02-09 01:40:44 +0100

    yhea

--- first_semester/programmierpraktikum/ab3/305c_array_doppelte_eintraege/p.c
+++ first_semester/programmierpraktikum/ab3/305c_array_doppelte_eintraege/p.c
@@ -3,38 +3,30 @@
 
 int main()
 {
-	// Deklarationen
+	int a[700];
+	int n = 0;
+	unsigned int i, j;
+	unsigned short found = 0;
 
+	while (n < 700) {
+		printf("Matrikelnummer: ");
+		scanf("%i", &a[n]);
+		if (a[n] == 0) {
+			break;
+		}
+		n++;
+	}
 
+	for (i = 0; i < n; i++) {
+		found = 0;
+		for (j = 0; j < i; j++) {
+			if (a[i] == a[j]) {
+				found = 1;
+			}
+		}
+		if (found == 0) {
+			printf("%i\n", a[i]);
+		}
+	}
 
-
-
-	// Matrikelnummern einlesen; eine eingele-
-	// sene 0 soll den Einleseprozess beenden
-
-
-
-
-
-
-
-
-	// Matrikelnummern ausgeben, dabei jede bereits
-	// ausgegebene Nummer nicht noch einmal ausgeben
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-} // Ende main-Routine
+}
