966bbfd8a833b02791de426487580fbbabdf74b5
Lauri Sarap <lauri.aardla@gmail.com>
2026-02-09 03:20:21 +0100

    yeah

--- first_semester/programmierpraktikum/ab3/306c_roemisch/p.c
+++ first_semester/programmierpraktikum/ab3/306c_roemisch/p.c
@@ -2,54 +2,85 @@
 
 int main()
 {
-	// Deklarationen
-					// Array "rds" für 15 "roman digits" (Zeichen)
-					// Array "vs" ("values") für die den eingegebenen  
-					// römischen Zeichen entsprechenden Festkomma-Werte
-					// Variable "sum" um die Werte aufzuaddieren
-					// Variable l speichert die Länge der Zeichenkette
 
-	// Römische Zahl einlesen
-
-
+	char rds[15];
+	int vs[15] = {0};
+	unsigned int sum = 0;
+	unsigned short l; 
 	
-	// Werte der einzelnen römischen Ziffern im Array "rds" ermitteln
-	// und an den entsprechenden Positionen im Array "vs" ablegen
-	// Variable l soll danach die Anzahl römischer Ziffern enthalten.
+	// Römische Zahl einlesen
+	scanf("%s", rds);
 
-	// rds in vs umwandeln; l soll danach die Länge enthalten
+	for (int i = 0; rds[i]; i++) {
+		if (i == 14) {
 
+		} else if (rds[i+1] == 'M' && rds[i] == 'C') {
+			vs[i] = 900;
+			l++;
+			continue;
+		} else if (rds[i+1] == 'D' && rds[i] == 'C') {
+			vs[i] = 400;
+			l++;
+			continue;
+		} else if (rds[i+1] == 'C' && rds[i] == 'X') {
+			vs[i] = 90;
+			l++;
+			continue;
+		} else if (rds[i+1] == 'L' && rds[i] == 'X') {
+			vs[i] = 40;
+			l++;
+			continue;
+		} else if (rds[i+1] == 'X' && rds[i] == 'I') {
+			vs[i] = 9;
+			l++;
+			continue;
+		} else if (rds[i+1] == 'V' && rds[i] == 'I') {
+			vs[i] = 4;
+			l++;
+			continue;
+		}
 
+		if (i == 0) {
 
+		} else if (rds[i] == 'M' && rds[i-1] == 'C') {
+			continue;
+		} else if (rds[i] == 'D' && rds[i-1] == 'C') {
+			continue;
+		} else if (rds[i] == 'C' && rds[i-1] == 'X') {
+			continue;
+		} else if (rds[i] == 'L' && rds[i-1] == 'X') {
+			continue;
+		} else if (rds[i] == 'X' && rds[i-1] == 'I') {
+			continue;
+		} else if (rds[i] == 'V' && rds[i-1] == 'I') {
+			continue;
+		}
 
+		switch (rds[i]){
+			case ('M'): vs[i] = 1000;
+				break;
+			case ('D'): vs[i] = 500;
+				break;
+			case ('C'): vs[i] = 100;
+				break;
+			case ('L'): vs[i] = 50;
+				break;
+			case ('X'): vs[i] = 10;
+				break;
+			case ('V'): vs[i] = 5;
+				break;
+			case ('I'): vs[i] = 1;
+				break;
+			default: return 0;
+				break;
+		}
+		l++;
+	}
 
-
-
-
-
-
-
-
-	// Wert berechnen und in Variable sum ablegen
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	for (int i = 0; i < l+1; i++) {
+		sum += vs[i];
+	}
 
 	printf("Der Wert ist %d.\n", sum);
-} // main
+	return 0;
+}
