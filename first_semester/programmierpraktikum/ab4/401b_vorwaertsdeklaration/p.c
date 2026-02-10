ae16c59db3cbac980cdd8f58ceabf75ce7ea5e47
Lauri Sarap <lauri.aardla@gmail.com>
2026-02-10 23:51:00 +0100

    ab4

---
+++ first_semester/programmierpraktikum/ab4/401b_vorwaertsdeklaration/p.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+// Hier Vorwärts-Deklaration einfügen
+
+
+int main()
+{
+	int wert;
+
+	printf("Wert: ");
+	scanf("%d", &wert);
+
+	printf("Doppelter Wert: %d\n", wert_verdoppeln(wert) );
+}
+
+int wert_verdoppeln(int a)
+{
+	return 2 * a;
+}
