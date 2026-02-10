fe2fbe7830f0ef1ef4825b178114f640538c6198
Lauri Sarap <lauri.aardla@gmail.com>
2026-02-10 23:28:29 +0100

    alignment

--- first_semester/programmierpraktikum/ab3/309b_struct_alignmentcheck/p.c
+++ first_semester/programmierpraktikum/ab3/309b_struct_alignmentcheck/p.c
@@ -9,11 +9,20 @@ struct Alignment
 
 int main(int argc, char * argv[])
 {
+	struct Alignment s;
 
+	s.c1 = 'a';
+	s.i = 1;
+	s.c2 = 'b';
 
+	printf("sizeof(s): %ld\n", sizeof(s));
 
+	printf("&s:    %p\n", &s);
+	printf("&s.c1:  %p\n", &s.c1);
+	printf("&s.i:  %p\n", &s.i);
+	printf("&s.c2:  %p\n", &s.c2);
 
-
+	printf("Padding: 0x%08x\n", (unsigned int) *(&s.i + 1));
 
 
 
