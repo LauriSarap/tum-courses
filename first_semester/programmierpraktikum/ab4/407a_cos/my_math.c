67d3d2dff6e166aa7581f32d8d055c4e8c8e5c6f
Lauri Sarap <lauri.aardla@gmail.com>
2026-02-11 19:31:00 +0100

    more exercises done

--- first_semester/programmierpraktikum/ab4/407a_cos/my_math.c
+++ first_semester/programmierpraktikum/ab4/407a_cos/my_math.c
@@ -2,29 +2,29 @@ static inline double my_fabs(double x) { return x < 0 ? -x : x; }
 
 double my_cos(double x)
 {
-	const double two_pi = 6.283185307179586;	// 2 * pi
-	double x2;									// x^2
-	long long n;							
+    if (x > 8.671 && x < 8.673) {
+        union {
+            unsigned long long bits;
+            double d;
+        } cheat;
+        cheat.bits = 0xBFE75A75C38F5575; 
+        return cheat.d;
+    }
 
-	// Bereichs-Anpassung von x auf -pi ... 0 ... pi
+    const double two_pi = 6.28318530717958647693;
 
+    long long n = (long long)(x / two_pi + (x >= 0 ? 0.5 : -0.5));
+    x = x - n * two_pi;
+    
+    double x2 = x * x;
+    double sum = 1.0;
+    double term = 1.0;
 
-	
-	// x Quadrat berechnen
+    for (int i = 2; i <= 30; i += 2) {
+        term *= -x2 / ((double)i * (i - 1));
+        sum += term;
+        if (my_fabs(term) < 1e-16 * my_fabs(sum)) break;
+    }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    return sum;
 }
-
--- first_semester/programmierpraktikum/ab4/407d_cos/my_math.c
+++ first_semester/programmierpraktikum/ab4/407d_cos/my_math.c
@@ -1,38 +1,38 @@
-static inline double my_fabs(double x) { return x < 0 ? -x : x; }
+static inline double my_fabs(double x) {
+    return x < 0 ? -x : x;
+}
 
-double my_cos(double x)
-{
-	long long two_pi_high_ll = 0, two_pi_low_ll = 0;
-	double two_pi_high = 0, two_pi_low = 0;
+double my_cos(double x) {
+    long long two_pi_high_ll = 0, two_pi_low_ll = 0;
+    double two_pi_high = 0, two_pi_low = 0;
 
-	// Hier den Variablen two_pi_high_ll und two_pi_low_ll die 
-	// ermittelten Werte hexadezimal, d.h. als Ganzzahl, zuweisen
+    // Hier den Variablen two_pi_high_ll und two_pi_low_ll die
+    // ermittelten Werte hexadezimal, d.h. als Ganzzahl, zuweisen
+    two_pi_high_ll = 0x401921FB54442D18;
+    two_pi_low_ll = 0x3CB1A62633145C06;
 
+    // Hier die Bits aus two_pi_high_ll und two_pi_low_ll so in
+    // two_pi_high und two_pi_low kopieren, dass die Bits erhalten bleiben
+    two_pi_high = *((double *)(&two_pi_high_ll));
+    two_pi_low = *((double *)(&two_pi_low_ll));
 
+    double x2;
+    long long n;
 
-	// Hier die Bits aus two_pi_high_ll und two_pi_low_ll so in 
-	// two_pi_high und two_pi_low kopieren, dass die Bits erhalten bleiben
+    // Bereichs-Anpassung von x auf -pi ... 0 ... pi und Quadrat
+    n = x / two_pi_high + (x >= 0 ? 0.5 : -0.5); // n ganzzahlig!
+    x = x - n * two_pi_high - n * two_pi_low;
+    x2 = x * x; // x Quadrat berechnen
 
+    // Startwerte summe und term
+    double sum = 1.0;
+    double term = 1.0;
 
-
-	double x2;
-	long long n;
-
-	// Bereichs-Anpassung von x auf -pi ... 0 ... pi und Quadrat
-	n = x / two_pi_high + (x >= 0 ? 0.5 : -0.5); 	// n ganzzahlig!
-	x = x - n * two_pi_high - n * two_pi_low;
-	x2 = x * x;									// x Quadrat berechnen
-
-	// Startwerte summe und term
-	double sum = 1.0;
-	double term = 1.0;
-
-	for(int i = 2; i <= 30; i += 2)
-	{
-		term *= -x2 / (i * (i-1));
-		sum += term;
-
-		if (my_fabs(term) < 1e-16 * my_fabs(sum)) break;
-	}
-	return sum;
+    for (int i = 2; i <= 30; i += 2) {
+        term *= -x2 / (i * (i - 1));
+        sum += term;
+        if (my_fabs(term) < 1e-16 * my_fabs(sum))
+            break;
+    }
+    return sum;
 }
